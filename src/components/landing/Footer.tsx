636cca3665b8cfd54176de24bf138dc0be0a9be7
KaIawinka <standofferm555@gmail.com>
2026-09-22 18:03:03 +0600

    Restore original three social buttons

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -7,16 +7,16 @@ interface FooterProps {
   locale: Locale;
 }
 
+function WhatsAppMark() {
+  return <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5 fill-current"><path d="M12 2a9.9 9.9 0 0 0-8.56 14.87L2 22l5.3-1.39A9.9 9.9 0 1 0 12 2Zm0 18a8.1 8.1 0 0 1-4.13-1.13l-.3-.18-3.15.83.84-3.07-.2-.32A8.1 8.1 0 1 1 12 20Zm4.43-6.06c-.24-.12-1.43-.7-1.65-.78-.22-.08-.38-.12-.54.12-.16.24-.62.78-.76.94-.14.16-.28.18-.52.06-.24-.12-1-.37-1.91-1.18-.71-.63-1.19-1.41-1.33-1.65-.14-.24-.01-.37.1-.49.11-.11.24-.28.36-.42.12-.14.16-.24.24-.4.08-.16.04-.3-.02-.42-.06-.12-.54-1.31-.74-1.79-.2-.47-.4-.41-.54-.42h-.46c-.16 0-.42.06-.64.3-.22.24-.84.82-.84 2s.86 2.32.98 2.48c.12.16 1.69 2.58 4.09 3.62.57.25 1.01.4 1.36.51.57.18 1.09.15 1.5.09.46-.07 1.43-.58 1.63-1.14.2-.56.2-1.04.14-1.14-.06-.1-.22-.16-.46-.28Z" /></svg>;
+}
+
 function InstagramMark() {
   return <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5 fill-current"><path d="M7.5 2h9A5.5 5.5 0 0 1 22 7.5v9a5.5 5.5 0 0 1-5.5 5.5h-9A5.5 5.5 0 0 1 2 16.5v-9A5.5 5.5 0 0 1 7.5 2Zm0 1.8A3.7 3.7 0 0 0 3.8 7.5v9a3.7 3.7 0 0 0 3.7 3.7h9a3.7 3.7 0 0 0 3.7-3.7v-9a3.7 3.7 0 0 0-3.7-3.7h-9Zm9.25 1.35a1.1 1.1 0 1 1 0 2.2 1.1 1.1 0 0 1 0-2.2ZM12 6.8A5.2 5.2 0 1 1 6.8 12 5.2 5.2 0 0 1 12 6.8Zm0 1.8A3.4 3.4 0 1 0 15.4 12 3.4 3.4 0 0 0 12 8.6Z" /></svg>;
 }
 
-function FacebookMark() {
-  return <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5 fill-current"><path d="M13.5 21v-8h2.7l.4-3h-3.1V8.08c0-.87.24-1.46 1.5-1.46h1.7V3.94c-.3-.04-1.31-.14-2.5-.14-2.47 0-4.16 1.5-4.16 4.27V10H7.3v3h2.74v8h3.46Z" /></svg>;
-}
-
-function XMark() {
-  return <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5 fill-current"><path d="M18.9 2H22l-6.8 7.8L23.2 22h-6.4l-5-6.5L6.1 22H3l7.3-8.4L2.4 2h6.5l4.5 5.9L18.9 2Zm-1.1 17.5h1.8L7.9 4.4H6L17.8 19.5Z" /></svg>;
+function TelegramMark() {
+  return <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5 fill-current"><path d="M21.7 3.6 18.6 20c-.23 1.16-.84 1.45-1.7.9l-4.7-3.46-2.27 2.18c-.25.25-.46.46-.94.46l.34-4.8 8.74-7.9c.38-.34-.08-.53-.59-.19L6.67 13.97l-4.65-1.46c-1.01-.32-1.03-1.01.21-1.5L20.4 3.02c.84-.31 1.58.19 1.3.58Z" /></svg>;
 }
 
 export function Footer({ t }: FooterProps) {
@@ -42,24 +42,10 @@ export function Footer({ t }: FooterProps) {
 
         <div className="flex flex-col gap-5 border-t border-white/10 pt-6 text-sm text-white/40 sm:flex-row sm:items-center sm:justify-between">
           <div className="flex flex-wrap items-center gap-3"><span>© {new Date().getFullYear()} CaféFlow.</span><span>{t.footer.rights}</span></div>
-          <div className="cafeflow-social-card" aria-label="CaféFlow в социальных сетях">
-            <ul className="cafeflow-social-card__list">
-              <li className="cafeflow-social-card__item">
-                <span /><span /><span />
-                <a aria-label="Facebook" href="https://facebook.com" className="cafeflow-social-card__link text-[#6ea8fe]"><FacebookMark /></a>
-                <span className="cafeflow-social-card__text">Facebook</span>
-              </li>
-              <li className="cafeflow-social-card__item">
-                <span /><span /><span />
-                <a aria-label="X / Twitter" href="https://x.com" className="cafeflow-social-card__link text-white/80"><XMark /></a>
-                <span className="cafeflow-social-card__text">Twitter</span>
-              </li>
-              <li className="cafeflow-social-card__item">
-                <span /><span /><span />
-                <a aria-label="Instagram" href="https://instagram.com" className="cafeflow-social-card__link text-[#fb7185]"><InstagramMark /></a>
-                <span className="cafeflow-social-card__text">Instagram</span>
-              </li>
-            </ul>
+          <div className="flex items-center gap-2">
+            <a aria-label="WhatsApp" href="https://wa.me/996555000000" className="flex h-9 w-9 items-center justify-center rounded-md border border-[#25d366]/40 text-[#25d366] transition hover:border-[#25d366] hover:bg-[#25d366] hover:text-white"><WhatsAppMark /></a>
+            <a aria-label="Instagram" href="https://instagram.com" className="flex h-9 w-9 items-center justify-center rounded-md border border-[#e4405f]/40 text-[#e4405f] transition hover:border-[#e4405f] hover:bg-[#e4405f] hover:text-white"><InstagramMark /></a>
+            <a aria-label="Telegram" href="https://t.me" className="flex h-9 w-9 items-center justify-center rounded-md border border-[#229ed9]/40 text-[#229ed9] transition hover:border-[#229ed9] hover:bg-[#229ed9] hover:text-white"><TelegramMark /></a>
           </div>
         </div>
       </div>
